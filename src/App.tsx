e1571b0727fe614fcaa1cab44f6f4bfae4bd8e8b
nimo <nishantmogha1234@gmail.com>
2025-01-02 00:36:05 +0530

    add win state and move history

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,10 @@ export default function App() {
     const timer = setInterval(() => {
       if (chess.turn() === "w") setTimerWhite((curr) => curr - 1)
       else setTimerBlack((curr) => curr - 1)
+      if (chess.isCheckmate() || chess.isDraw()) {
+        setGameState("result")
+        clearInterval(timer)
+      }
     }, 1000)
     return () => clearInterval(timer)
   }, [gameState])
@@ -94,8 +98,67 @@ export default function App() {
           </div>
         </div>
       )}
+      {gameState === "result" && (
+        <div className="fixed inset-0 z-50 bg-black/40 flex justify-center items-center">
+          <div className="p-8 rounded-lg bg-white">
+            <h2 className="mb-4 flex gap-2 items-center justify-center text-lg font-bold">
+              {chess.isDraw()
+                ? "Draw"
+                : chess.turn() === "w"
+                ? "Black Wins"
+                : "White Wins"}
+              !
+            </h2>
+            <div className="flex justify-around gap-4 mb-4">
+              <button
+                onClick={() => changeGameType("rapid")}
+                className={cn(
+                  "py-2 px-4 border-2 font-medium shadow rounded-md",
+                  gameType === "rapid" &&
+                    "border-lime-500 bg-lime-50 [&>b]:text-lime-700"
+                )}
+              >
+                <div className="text-lg">10 min</div>
+                <b>Rapid</b>
+              </button>
+              <button
+                onClick={() => changeGameType("blitz")}
+                className={cn(
+                  "py-2 px-4 border-2 font-medium shadow rounded-md",
+                  gameType === "blitz" &&
+                    "border-lime-500 bg-lime-50 [&>b]:text-lime-700"
+                )}
+              >
+                <div className="text-lg">5 min</div>
+                <b>Blitz</b>
+              </button>
+              <button
+                onClick={() => changeGameType("bullet")}
+                className={cn(
+                  "py-2 px-4 border-2 font-medium shadow rounded-md",
+                  gameType === "bullet" &&
+                    "border-lime-500 bg-lime-50 [&>b]:text-lime-700"
+                )}
+              >
+                <div className="text-lg">2 min</div>
+                <b>Bullet</b>
+              </button>
+            </div>
+
+            <button
+              onClick={() => {
+                chess.reset()
+                setGameState("ingame")
+              }}
+              className="w-full flex gap-4 items-center bg-lime-600 py-4 px-8 font-medium text-white text-2xl shadow rounded"
+            >
+              <PlayIcon width={32} height={32} /> Start New Game
+            </button>
+          </div>
+        </div>
+      )}
       <ChessBoard />
-      <div className="self-stretch flex flex-col justify-between m-4  text-xl">
+      <div className="self-stretch flex flex-col justify-between m-4 text-xl">
         <div className="bg-white shadow rounded-lg p-4">
           <div
             className={cn(
@@ -108,6 +171,23 @@ export default function App() {
             {new Date(timerBlack * 1000).toISOString().slice(14, 19)}
           </div>
         </div>
+        <ul className="flex flex-col-reverse gap-2 max-h-[75vh] bg-white rounded-lg flex-1 my-2 p-4 shadow overflow-y-auto">
+          {chess.history().map((move, i) => (
+            <li
+              key={move}
+              className="flex gap-2 items-center px-4 py-1 rounded-md shadow bg-white border"
+            >
+              <img
+                className="size-6"
+                src={`/assets/chess-pieces/${
+                  (i % 2 === 0 ? "w" : "b") +
+                  (move.length === 2 ? "p" : move[0].toLowerCase())
+                }.svg`}
+              />
+              {move}
+            </li>
+          ))}
+        </ul>
         <div className="bg-white shadow rounded-lg p-4">
           <div
             className={cn(
